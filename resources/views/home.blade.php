0d751242fca4397ff6b6dcef79db699dc417fd07
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-09 17:51:37 +0200

    feat: added badges print for cards - home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,23 +13,57 @@
 
                   <div>
 
-                     <div>
+                     <div class="position-relative">
 
-                        <img src="{{ Vite::asset('resources/img/' . $product['frontImage'])}}" alt="Image of {{$product['name']}}">
+                        <img src="{{ Vite::asset('resources/img/' . $product['frontImage']) }}"
+                             alt="Image of {{ $product['name'] }}">
+
+                        <div class="favorite-badges bg-white">
+
+                           @if ($product['isInFavorites'])
+                              <i class="fa-solid fa-heart p-2"></i>
+                           @else
+                              <i class="fa-regular fa-heart p-2"></i>
+                           @endif
+
+                        </div>
+
+                        @if ($product['badges'])
+
+                           <div class="optional-badges">
+
+                              <div class="text-white fw-bold gap-1">
+                                 @foreach ($product['badges'] as $badge)
+                                    @if ($badge['type'] === 'discount')
+                                       <span class="bg-danger px-1 me-1"> {{ $badge['value'] }} </span>
+                                    @endif
+                                 @endforeach
+
+                                 @foreach ($product['badges'] as $badge)
+                                    @if ($badge['type'] === 'tag')
+                                       <span class="bg-success px-1"> {{ $badge['value'] }} </span>
+                                    @endif
+                                 @endforeach
+
+                              </div>
+
+                           </div>
+                        @endif
 
                      </div>
 
-                     <div>
 
-                        <p class="fs-6">{{$product['brand']}}</p>
+                  </div>
 
-                        <h3 class="fs-3">{{strtoupper($product['name'])}}</h3>
+                  <div>
 
-                        <div class="fs-6 fw-bold">
+                     <em class="fs-6">{{ $product['brand'] }}</em>
 
-                           <span class="text-danger">{{$product['price']}} €</span>
+                     <h3 class="fs-3">{{ strtoupper($product['name']) }}</h3>
 
-                        </div>
+                     <div class="fs-6 fw-bold">
+
+                        <span class="text-danger">{{ $product['price'] }} €</span>
 
                      </div>
 
